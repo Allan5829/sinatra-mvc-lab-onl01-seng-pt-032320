9caf422efcb7cfa39b318d676a4aa7987f827cab
Allan Farca <allan.farca@gmail.com>
2020-06-19 10:16:03 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,11 +5,24 @@ class PigLatinizer
     @input = input.downcase
   end
 
-  def latinizer
+  def latin_whole_input(input)
+    input_array = input.split(" ")
+    output = []
+
+    input_array.each do |words|
+      output << latinizer(words)
+    end
+
+    output_array = output.join(" ")
+    output_array
+
+  end
+
+  def latinizer(old_word)
     suffix_ay = "ay"
     suffix_way = "way"
 
-    letters_array = @input.split
+    letters_array = old_word.split
 
     new_letters_array = check_if_vowel(letters_array)
 
@@ -37,47 +50,4 @@ class PigLatinizer
     end
   end
 
-end
-
-class TextAnalyzer
-  attr_reader :text
-
-  def initialize(text)
-    @text = text.downcase
-  end
-
-  def count_of_words
-    words = text.split(" ")
-    words.count
-  end
-
-  def count_of_vowels
-    text.scan(/[aeoui]/).count
-  end
-
-  def count_of_consonants
-    text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
-  end
-
-  def most_used_letter
-    s1 = text.gsub(/[^a-z]/, '') # gets rid of spaces
-    arr = s1.split('')
-    arr1 = arr.uniq
-    arr2 = {}
-
-    arr1.map do |c|
-      arr2[c] =  arr.count(c)
-    end
-
-    biggest = { arr2.keys.first => arr2.values.first }
-
-    arr2.each do |key, value|
-      if value > biggest.values.first
-        biggest = {}
-        biggest[key] = value
-      end
-    end
-
-    biggest
-  end
 end
